62f2c80ed28bcfd16045bf5b1f4d06a87d089335
l-bevc <lbevc@pmfst.hr>
2018-11-27 09:51:11 +0100

    Changed Program.cs

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
+            Console.WriteLine("Ovo je Intership");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
